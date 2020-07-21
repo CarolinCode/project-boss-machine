a5ff7bcf007e7394d5e94290f156edb37fcabe45
CarolinCode <61985209+CarolinCode@users.noreply.github.com>
2020-07-21 17:06:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,28 +8,21 @@ const morgan = require('morgan');
 
 module.exports = app;
 
-/* Do not change the following line! It is required for testing and allowing
-*  the frontend application to interact as planned with the api server
-*/
 const PORT = process.env.PORT || 4001;
 
-// Add middleware for handling CORS requests from index.html
 app.use(cors());
 
-// Add middware for parsing request bodies here:
 app.use(bodyParser.json());
 
-// extra added morgan for logging, but where can I see it?
 app.use(morgan('tiny'));
 
-// Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
 app.use('/api', apiRouter);
 
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
-  // Add your code to start the server listening at PORT below:
+  
 app.listen(PORT, () => {
   console.log(`Listening for requests at port ${PORT}`);
 });
